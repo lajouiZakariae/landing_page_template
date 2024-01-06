0599bff814c543d8525519c9ea34dc02d48cf600
Lajoui Zakariae <lajoui.zakariae.1@gmail.com>
2024-01-06 19:33:00 +0100

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 import { defineConfig } from 'astro/config';
-
+import tailwind from '@astrojs/tailwind';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// https://astro.build/config
 export default defineConfig({
   output: 'static',
-
-  integrations: [],
+  integrations: [tailwind()],
   vite: {
     resolve: {
       alias: {
