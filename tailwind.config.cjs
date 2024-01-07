c50b61bd773abc7252e50b711f79ef8af699f553
Lajoui Zakariae <lajoui.zakariae.1@gmail.com>
2024-01-07 13:32:35 +0100

    sass vars

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
   theme: {
+    container: { center: true },
     extend: {
       colors: {
         primary: '#2196f3',
+      },
+      fontSize: {
+        14: '14px',
+        18: '18px',
+      },
+      borderWidth: {
+        1: '1px',
       },
     },
   },
